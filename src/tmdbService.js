465f63615b7ea5ab4772cb4f52658cbf54c08155
topnazer <147312524+topnazer@users.noreply.github.com>
2024-08-30 11:43:23 +0800

    Update tmdbService.js

--- src/tmdbService.js
+++ src/tmdbService.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-const API_KEY = '8bf433e4856105461ea85fcffa5a298e'; // Replace with your TMDB API key
-const BASE_URL = 'https://api.themoviedb.org/3';
+// Replace with your TMDB API key
+const API_KEY = '8bf433e4856105461ea85fcffa5a298e'; 
+const BASE_URL = 'https://api.themoviedb.org/3'; // Base URL for the TMDB API
 
+// Create an axios instance with base URL and API key
 const tmdb = axios.create({
   baseURL: BASE_URL,
   params: {
@@ -10,6 +12,7 @@ const tmdb = axios.create({
   },
 });
 
+// Function to search for movies based on a query
 export const searchMovies = (query) => {
   return tmdb.get(`/search/movie`, {
     params: {
@@ -18,10 +21,12 @@ export const searchMovies = (query) => {
   });
 };
 
+// Function to get trending movies
 export const getTrendingMovies = () => {
   return tmdb.get(`/trending/movie/week`);
 };
 
+// Function to get details of a specific movie by its ID
 export const getMovieDetails = (movieId) => {
   return tmdb.get(`/movie/${movieId}`);
 };
